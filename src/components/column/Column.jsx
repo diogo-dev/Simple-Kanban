2e2c3f6b75df1a0beeccae652fa0e4b7631b91bd
Diogo Felipe <diogofelipe.dev@gmail.com>
2025-03-20 17:01:07 -0300

    Highlighting the indicators

--- src/components/column/Column.jsx
+++ src/components/column/Column.jsx
@@ -14,15 +14,59 @@ const Column = ({ title, headingColor, column, cards, setCards }) => {
 
   const handleDragOver = (e) => {
     e.preventDefault();
+    highlightIndicator(e);
     setActive(true);
   };
 
+  const highlightIndicator = (e) => {
+    const indicators = getIndicators();
+    clearHighlights(indicators);
+    const el = getNearestIndicator(e, indicators);
+    el.element.style.opacity = "1";
+  };
+
+  const clearHighlights = (els) => {
+    const indicators = els || getIndicators();
+
+    indicators.forEach((i) => {
+      i.style.opacity = "0";
+    });
+  };
+
+  const getNearestIndicator = (e, indicators) => {
+    const DISTANCE_OFFSET = 50;
+    const el = indicators.reduce(
+      (closest, child) => { 
+        const box = child.getBoundingClientRect();
+        const offset = e.clientY - (box.top + DISTANCE_OFFSET);
+
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      {
+        offset: Number.NEGATIVE_INFINITY,
+        element: indicators[indicators.length - 1],
+      }
+    );
+
+    return el;
+  };
+
+  const getIndicators = () => {
+    return Array.from(document.querySelectorAll(`[data-column="${column}"]`));
+  };
+  
   const handleDragLeave = () => {
     setActive(false);
+    clearHighlights();
   };
 
   const handleDragEnd = (e) => {
     setActive(false);
+    clearHighlights();
   };
 
   return (
